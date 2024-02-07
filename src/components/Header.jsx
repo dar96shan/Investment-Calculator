21c0473ec637b28b08f66c7b0ebd7cdd13172605
darshan <darshankrishna96.dg96@gmail.com>
2024-02-06 23:35:58 -0800

    Working Investment calculator

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <header id="header">
+        <img src="/investment-calculator-logo.png" alt="investment-logo"></img>
+        <h1>Investment Calculator</h1>
+      </header>
+    </>
+  );
+}
